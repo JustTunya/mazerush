07ae39dd938e6854b9c29231654bdf0c2efee08e
Vidám Szabolcs <vdmszblcs@yahoo.com>
2025-10-21 20:39:33 +0300

    Added cell and maze drawing

--- src/game.pl
+++ src/game.pl
@@ -10,3 +10,18 @@ valid_move(Maze, N, X, Y) :-
     inside(N, X, Y),
     get_char(Maze, X, Y, Cell),
     Cell \= '#'.
+
+% draw_cell(Symbol, PlayerX, PlayerY, CellX, CellY)
+draw_cell(_, X, Y, X, Y):-
+    write('P'), !.
+draw_cell(Symbol, _, _, _, _):-
+    write(Symbol), !.
+
+draw_maze(Maze, N, PlayerX, PlayerY):-
+    forall(between(1, N, I),
+        (forall(between(1, N, J),
+            (get_char(Maze, I, J, Cell),
+            draw_cell(Cell, PlayerX, PlayerY, I, J))
+        ),
+        nl)
+    ).
