9af12345bee17a5196c75d27b72c98f49d57ead0
Vidám Szabolcs <vdmszblcs@yahoo.com>
2025-10-25 14:03:34 +0300

    Added possibility to change a cell in the generated grid

--- README.md
+++ README.md
@@ -4,4 +4,9 @@ MazeRush is a simple tile-based maze game written in **SWI-Prolog** using the **
 Created by **Vidám Szabolcs** and **Tunya Lénárd-Sándor**.
 
 ## Overview
+
 The goal of MazeRush is to escape from the randomly generated maze as fast as possible.
+
+## Documentation used
+
+%https://www.swi-prolog.org/pldoc/man?predicate=nth0/4
--- src/maze.pl
+++ src/maze.pl
@@ -1,8 +1,10 @@
+% First we make a grid full of walls, basically a maze you cannot walk anywhere
+
 % make_grid(+N, -Grid)
-make_grid(N, Grid):- % We are making a grid full of walls first
+make_grid(N, Grid):-
     make_rows(N, N, Grid). 
 
-% make_rows(+NumberOfRows, +NumberOfColumns, -Grid).
+% make_rows(+NumberOfRows, +NumberOfColumns, -Grid)
 make_rows(0, _, []).
 make_rows(RowIndex, N, [Row|Rest]):-
     RowIndex > 0,
@@ -23,4 +25,28 @@ make_row(N, ['#'|Rest]):-
 
 % ?- make_row(5, Sor).
 % Sor = [#, #, #, #, #] ;
+% false.
+
+% Now we want a function that changes a single cell in the maze given by an index
+
+% set_cell(+Grid, +RowIndex, +ColumnIndex, +NewCell, -NewGrid)
+set_cell(Grid, RowIndex, ColumnIndex, NewCell, NewGrid):- % here we actually dont change our grid but recreate it with a one-cell change
+    nth1(RowIndex, Grid, Row),
+    replace_in_list(Row, ColumnIndex, NewCell, NewRow),
+    replace_in_list(Grid, RowIndex, NewRow, NewGrid). % this works because maze is represented as an array of arrays 
+    
+% ?- make_grid(5, Grid), set_cell(Grid, 1, 1, x, NewGrid).
+% Grid = [[#, #, #, #, #], [#, #, #, #, #], [#, #, #, #, #], [#, #, #, #, #], [#, #, #, #|...]],
+% NewGrid = [[x, #, #, #, #], [#, #, #, #, #], [#, #, #, #, #], [#, #, #, #, #], [#, #, #, #|...]] ;
+% false.
+
+% replace_in_list(+List, +Index, +NewElement, -NewList)
+replace_in_list([_|Rest], 1, NewElement, [NewElement|Rest]). % 1 is just the stopping point of the countdown, because walk the list recursively
+replace_in_list([X|Rest], Index, NewElement, [X|NewRest]):-
+    Index > 1,
+    Index_prime is Index - 1,
+    replace_in_list(Rest, Index_prime, NewElement, NewRest).
+    
+% ?- replace_in_list([a,b,c,d], 3, x, NewList).
+% NewList = [a, b, x, d] ;
 % false.
